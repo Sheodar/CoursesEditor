3bee9ef78b76e5aae54a0e6e56383dfd4ae1e2ab
Sheodar <kkabachok@mail.ru>
2016-11-28 19:25:14 +0200

    Fix helper

--- src/methods/other/Helper.java
+++ src/methods/other/Helper.java
@@ -6,24 +6,24 @@ public class Helper {
     public static void helper() {
         System.out.println(
                   " _______________________________________________________________" +
-                "\n |- create course ----+- создать курс -------------------------|" +
-                "\n |- create student ---+- создать cтудента ---------------------|" +
-                "\n |- create trainer ---+- создать тренера ----------------------|" +
-                "\n |- create task N ----+- создать вопрос в курсе N -------------|" +
-                "\n |- show allstudents -+- показать всех студентов --------------|" +
-                "\n |- show alltrainers -+- показать всех тренеров ---------------|" +
-                "\n |- show course N ----+- показать курс с ID N -----------------|" +
-                "\n |- show student N ---+- показать студента с ID N -------------|" +
-                "\n |- show courses -----+- показать все курсы -------------------|" +
-                "\n |- show students N --+- показать всех студентов курса с ID N -|" +
-                "\n |- show trainer N ---+- показать тренера с ID N --------------|" +
-                "\n |- show journal N ---+- показать успеваемость курса с ID N ---|" +
-                "\n |- change student N -+- изменить параметры студента с ID N ---|" +
-                "\n |- change course N --+- изменить параметры курса с ID N ------|" +
-                "\n |- change trainer N -+- изменить параметры тренера с ID N ----|" +
-                "\n |- save journal N ---+- сохранить успеваемость курса с ID N --|" +
-                "\n |- exit -------------+- закрыть программу --------------------|" +
-                "\n |- help -------------+- помощь(показать все команды) ---------|" +
+                "\n |- create course ----+- create course -------------------------|" +
+                "\n |- create student ---+- create students -----------------------|" +
+                "\n |- create trainer ---+- create trainer ------------------------|" +
+                "\n |- create task N ----+- create task on course X ---------------|" +
+                "\n |- show allstudents -+- show all students ---------------------|" +
+                "\n |- show alltrainers -+- show all trainers ---------------------|" +
+                "\n |- show course N ----+- show course ID X ----------------------|" +
+                "\n |- show student N ---+- show student ID X ---------------------|" +
+                "\n |- show courses -----+- show all courses ----------------------|" +
+                "\n |- show students N --+- show all student on course ID N -------|" +
+                "\n |- show trainer N ---+- show trainer ID N ---------------------|" +
+                "\n |- show journal N ---+- show journal course ID N --------------|" +
+                "\n |- change student N -+- change student ID N -------------------|" +
+                "\n |- change course N --+- change course ID N --------------------|" +
+                "\n |- change trainer N -+- change trainer ID N -------------------|" +
+                "\n |- save journal N ---+- save in file journal course ID N ------|" +
+                "\n |- exit -------------+- close program -------------------------|" +
+                "\n |- help -------------+- help(show all commands) ---------------|" +
                 "\n _______________________________________________________________"
         );
     }
